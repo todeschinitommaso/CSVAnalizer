6be5796ba1eaa8520010729daefb7fecbb6db7ac
Todeschini.19331 <Todeschini.19331@LABI1-6PC08>
2022-09-19 09:16:01 +0200

    Aggiungere i file di progetto.

---
+++ CSVAnalizer/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSVAnalizer
+{
+    public partial class Form1 : Form
+    {
+        public string path = @".\Dati.csv";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //CONTROLLO LUNGHEZZA RECORD
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int character;
+            int prevchar = 0;
+            StreamReader sr = new StreamReader(path);
+            int count = 0;
+            string check = "Dimensioni Uguali";
+
+            while (sr.ReadLine() != null)
+            {
+                character = sr.ReadLine().Length;
+
+                if(count != 0)
+                {
+                    if(prevchar != character)
+                    {
+                        check = "Dimensioni differenti";
+                        MessageBox.Show(check);
+                        return;
+                    }
+                }
+
+                prevchar = character;
+                count++;
+            }
+            MessageBox.Show(check);
+            sr.Close();
+        }
+
+    }
+}
